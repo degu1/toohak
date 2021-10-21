85b0e40ae2d55f52efcaf399b834fc17662c235f
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-10-21 09:17:12 +0200

    Added MyQuizes and connected to with fetch.

---
+++ src/views/MyQuizes.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+  <h1>My Quizes</h1>
+
+  <ul>
+    <li v-for="quiz in quizes" v-bind:key="quiz.quizId">{{quiz.quizName}}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyQuizes",
+  data: function(){
+    return {
+      quizes: []
+    }
+  },
+  mounted(){
+    fetch('http://127.0.0.1:3000/quiznames')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data.quizes);
+          this.quizes = data.quizes;
+        });
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
