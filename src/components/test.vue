be942912e82882b68acbf380f1f3678e64f7857f
Milad Nazari <milad.nazari@live.se>
2021-10-26 15:37:44 +0200

    Connected with database with fetch, and printed out information about quiz.

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="question in questions" v-bind:key="question.question_id">{{ question }}</li>
+    </ul>
+    <h1>{{id}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    id: String
+  },
+  data: function () {
+    return {
+      questions: [],
+      testId: this.id
+    }
+  },
+  mounted() {
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
