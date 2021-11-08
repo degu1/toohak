305a288ab5e04e9d62cb60289e83f67c7eaac020
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-11-08 11:52:54 +0100

    * Added css for adding quizes and questions.

--- src/views/AddQuiz.vue
+++ src/views/AddQuiz.vue
@@ -1,37 +1,53 @@
 <template>
   <div id="questionContainer">
-    <h1 id="quizName">Add new quiz</h1>
-    <form v-on:submit.prevent="addNewQuiz">
-      <label for="quizName">Quiz Name</label>
-      <input type="text" v-model="quizName">
-      <button v-on:click="updateQuizes()">Add quiz</button>
-    </form>
-    <ul>
-      <li v-for="quiz in quizes" v-bind:key="quiz.quiz_id" v-on:click="setActiveQuizId(quiz), getQuestions()">
-        {{ quiz.quiz_name }}
-      </li>
-    </ul>
-    <form v-on:submit.prevent="addNewQuestion">
-      <p>Quiz id: {{ activeQuizId }}</p>
-      <label for="questionName">Question</label>
-      <input type="text" id="questionName" v-model="question">
 
-      <label for="choices">Choices</label>
-      <input type="text" placeholder="Choice 1" id="choices" v-model="choiceOne">
-      <input type="text" placeholder="Choice 2" v-model="choiceTwo">
-      <input type="text" placeholder="Choice 3" v-model="choiceThree">
-      <input type="text" placeholder="Choice 4" v-model="choiceFour">
+    <div v-if="activeQuizName == ''" class="addQuizContainer">
+      <h1 id="quizName">Add new quiz</h1>
+      <form v-on:submit.prevent="addNewQuiz" id="addNewQuiz">
 
-      <label for="rightAnswer">Right answer</label>
-      <input type="text" id="rightAnswer" v-model="rightAnswer">
+        <input type="text" v-model="quizName" placeholder="Quiz name...">
 
-      <button>Add question</button>
-    </form>
-    <ul>
-      <li v-for="question in activeQuestions" v-bind:key="question.question_id">
-        {{question.question}}
-      </li>
-    </ul>
+        <button v-on:click="updateQuizes()">Add quiz</button>
+      </form>
+      <h2>Your quizes</h2>
+      <ul class="quizList">
+        <li v-for="quiz in quizes" v-bind:key="quiz.quiz_id"
+            v-on:click="setActiveQuizId(quiz), getQuestions()">
+          {{ quiz.quiz_name }}
+          <img src="../assets/remove-btn.png" v-on:click="removeQuiz(quiz.quiz_id)">
+        </li>
+      </ul>
+    </div>
+    <div v-if="activeQuizName!= ''" class="addQuizContainer">
+      <form class="addQuestion" v-on:submit.prevent="addNewQuestion">
+        <h2>{{this.activeQuizName}}</h2>
+        <section class="formAddItemContainer">
+        <label for="questionName"></label>
+        <input type="text" id="questionName" v-model="question" placeholder="Question...">
+        </section>
+
+        <section class="formAddItemContainer">
+          <input type="text" placeholder="Choice 1" id="choices" v-model="choiceOne">
+          <input type="text" placeholder="Choice 2" v-model="choiceTwo">
+          <input type="text" placeholder="Choice 3" v-model="choiceThree">
+          <input type="text" placeholder="Choice 4" v-model="choiceFour">
+        </section>
+
+        <section class="formAddItemContainer">
+        <input type="text" id="rightAnswer" v-model="rightAnswer" placeholder="Right answer...">
+        </section>
+
+        <button>Add question</button>
+      </form>
+      <button v-on:click="activeQuizName=''">Back</button>
+      <h2>Questions</h2>
+      <ul class="formAddItemContainer" id="questionListContainer">
+        <li id="questionList" v-for="question in activeQuestions" v-bind:key="question.question_id">
+          {{ question.question }}
+          <img src="../assets/remove-btn.png" v-on:click="removeQuestion(question.question_id)">
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
@@ -106,23 +122,22 @@ export default {
       this.insertChoices()
 
 
+      var question = {
+        quiz_id: this.activeQuizId,
+        question: this.question,
+        correct_answer: this.rightAnswer,
+        answers: [{"answer": this.choiceOne}, {"answer": this.choiceTwo}, {"answer": this.choiceThree}, {"answer": this.choiceFour}]
+      }
+      var jsonQuestion = JSON.stringify(question);
+      console.log(jsonQuestion)
+      fetch('http://127.0.0.1:3000/quiz_question/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: jsonQuestion
+      })
 
-        var question = {
-          quiz_id: this.activeQuizId,
-          question: this.question,
-          correct_answer: this.rightAnswer,
-          answers: [{"answer": this.choiceOne}, {"answer": this.choiceTwo}, {"answer": this.choiceThree}, {"answer": this.choiceFour}]
-        }
-        var jsonQuestion = JSON.stringify(question);
-        console.log(jsonQuestion)
-        fetch('http://127.0.0.1:3000/quiz_question/', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: jsonQuestion
-        })
 
-
-        setTimeout(this.getQuestions,10)
+      setTimeout(this.getQuestions, 10)
       this.question = ''
       this.choiceOne = ''
       this.choiceTwo = ''
@@ -133,7 +148,7 @@ export default {
     setActiveQuizId: function (quiz) {
       this.activeQuizId = quiz.quiz_id
     },
-    getQuestions: function (){
+    getQuestions: function () {
       fetch('http://127.0.0.1:3000/questions/' + this.activeQuizId)
           .then((response) => {
             return response.json();
@@ -150,8 +165,118 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
-@import '../assets/css/activeQuiz.css';
+#questionContainer {
+  padding: 100px 10px;
+  color: #17252A;
+  display: grid;
+}
+
+.addQuizContainer {
+  display: grid;
+  flex-direction: column;
+  border-radius: 20px;
+}
+
+button {
+  margin: 10px 20px;
+  border-radius: 10px;
+  background-color: #17252A;
+  color: white;
+  justify-self: center;
+  width: fit-content;
+  font-weight: lighter;
+  padding: 10px 30px;
+  border-style: none;
+  cursor: pointer;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+.quizList {
+  cursor: pointer;
+  display: grid;
+  justify-self: center;
+  width: 100%;
+}
+
+.quizList li {
+  display: grid;
+  grid-template-columns: 1fr 10px;
+  background-color: white;
+  padding: 10px 20px;
+  width: 50%;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  justify-self: center;
+}
+
+.addQuestion {
+  display: flex;
+  flex-direction: column;
+}
+
+.formAddItemContainer {
+  display: grid;
+  margin-top: 15px;
+}
+
+input {
+  margin: 5px;
+  width: 50%;
+  justify-self: center;
+  padding: 10px 20px;
+  border-radius: 20px;
+  border: none;
+  font-weight: 300;
+}
+
+#addNewQuiz {
+  display: grid;
+}
+
+.addQuestion {
+  display: grid;
+}
+
+h1 {
+  margin-bottom: 10px;
+}
+
+h2 {
+  color: white;
+  margin-bottom: 0;
+}
+
+textarea:focus, input:focus{
+  outline: none;
+}
+
+#questionListContainer {
+  background-color: white;
+  border-radius: 20px;
+  padding: 20px;
+  width: 50%;
+  gap: 10px;
+  justify-self: center;
+}
+
+#questionList {
+  display: grid;
+  grid-template-columns: 1fr 10px;
+}
+
+/* Tablet */
+@media screen and (min-width: 768px) {
+  .addQuizContainer {
+    justify-self: center;
+    width: 100%;
+    max-width: 700px;
+  }
+}
 
 </style>
